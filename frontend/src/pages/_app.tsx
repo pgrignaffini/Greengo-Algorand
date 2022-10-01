310666aabec7ea063fc232a99e64ba46b3b93e62
grignaffini <grignaffinipaolo@gmail.com>
2022-10-01 12:26:44 +0200

    added project page

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -119,7 +119,7 @@ const MyApp: AppType = ({
 
 const getBaseUrl = () => {
   if (typeof window !== "undefined") return ""; // browser should use relative url
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+  if (process.env.NETLIFY_URL) return `https://${process.env.NETLIFY_URL}`; // SSR should use vercel url
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 };
 
